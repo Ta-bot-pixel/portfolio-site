bde33af6be0ceed6dd6f9e2aefa591d25c756fe2
Tony James <tonyjames19930330@gmail.com>
2025-10-23 03:38:24 -0300

    Link Update

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -7,12 +7,12 @@ export const personalData = {
   email: 'tonyjames19930330@gmail.com',
   phone: '+15513284644',
   address: 'Tampa City Florida, USA (Open to relocation)',
-  github: 'https://github.com/ta-bot-pixel',
-  facebook: 'https://www.facebook.com/Sushil.pc1/',
-  linkedIn: 'https://www.linkedin.com/in/sushilrajeeva/',
-  twitter: 'https://twitter.com/SushilRajeev',
-  stackOverflow: 'https://stackoverflow.com/users/21802231/sushil-rajeeva-bhandary',
-  leetcode: "https://leetcode.com/u/sushilrajeeva/",
+  github: '',
+  facebook: '',
+  linkedIn: '',
+  twitter: '',
+  stackOverflow: '',
+  leetcode: "",
   devUsername: "tonyJames",
   resume: "/Tony_James_Resume.docx"
 }
